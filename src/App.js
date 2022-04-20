ad0ef351cafeb920b9ecd35132c62254af0a8fde
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-20 23:25:28 +0600

    add user with form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import AddUser from "./components/AddUser/AddUser";
+import Home from "./components/Home/Home";
+import UpdateUser from "./components/UpdateUser/UpdateUser";
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/user/add" element={<AddUser></AddUser>}></Route>
+        <Route path="/update/:id" element={<UpdateUser></UpdateUser>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/UpdateUser/UpdateUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateUser = () => {
+  return (
+    <div>
+      <h2>Update User</h2>
+    </div>
+  );
+};
+
+export default UpdateUser;
